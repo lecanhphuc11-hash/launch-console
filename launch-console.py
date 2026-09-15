b63660a54f6cb51bae8a3e22c8017caae4d49b7d
lecanhphuc11-hash <lecanhphuc11@gmail.com>
2026-09-15 02:03:03 +0000

    odified Launch_console

--- launch-console.py
+++ launch-console.py
@@ -1,1 +1,18 @@
-wwwerwer
+name = "Canhphuc Le"
+print("Welcome to " + name + "'s Launch Console!")
+
+running = True
+while running:
+    print("1) About me")
+    print("2) My goals")
+    print("3) Exit")
+    choice = input("Pick 1-3: ")
+    if choice == "1":
+        print("I'm VP of My BEST Robotics Club and Intern in-training in C2C")
+    elif choice == "2":
+        print("My goal: ship my first real project this term and go to state for BEST Robotics")
+    elif choice == "3":
+        print("Goodbye!")
+        running = False
+    else:
+        print("Please pick 1, 2, or 3.")
